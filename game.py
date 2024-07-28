1f1086994f8c6de85d33e1eadd2e80b44613aa28
Thong <you@example.com>
2024-07-28 22:48:20 +0700

    upp

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame, sys ,random
 pygame.init()
-# tạo hàm cho game
+# method for game
 def double_floor():
         screen.blit(floor,(floor_x_position,600))
         screen.blit(floor,(floor_x_position+432,600))
@@ -17,33 +17,31 @@ def draw_pipe(pipes):
           screen.blit(pipe,p)
 screen = pygame.display.set_mode((432,768))
 clock = pygame.time.Clock() 
-# tạo biến cho trò chơi
+# ariables for game
 gravity = 0.25
 bird_move = 0
-# chèn background
+#  background
 bg = pygame.image.load('assets/background-night.png').convert()
 bg = pygame.transform.scale2x(bg)
-# chèn sàn
+# floor
 floor = pygame.image.load("assets/floor.png").convert()
 floor = pygame.transform.scale2x(floor)
-
-
-# lưu vị trí ban đầu 
+# original location 
 floor_x_position = 0
 
-# tạo con chim
+# bird
 bird = pygame.image.load("assets/yellowbird-midflap.png").convert()
 bird = pygame.transform.scale2x(bird)
 bird_react = bird.get_rect(center = (100,384))
-# tạo ống
+# pipe
 pipe = pygame.image.load("assets/pipe-green.png").convert()
 pipe = pygame.transform.scale2x(pipe)
 pipe_list = []
-# cho ống xuất hiện 1 time nhất định : timer
+# timer
 spawnpipe = pygame.USEREVENT
 pygame.time.set_timer(spawnpipe, 1200)
 pipe_height = [200,300,400]
-# vòng lặp sự kiện, lấy all event trong game diễn ra
+# loop event 
 while True:
     for event in pygame.event.get():
         # tạo phím ấn thoát game
@@ -58,7 +56,7 @@ while True:
         if event.type == spawnpipe:
              pipe_list.append(create_pipe())
              print(create_pipe)
-            # thêm hình ảnh lên màng hình (0,0 là gốc tọa độ d quy định trong pygame)
+            
     screen.blit(bg,(0,0))
     # Bird
     bird_move += gravity
@@ -67,12 +65,12 @@ while True:
     # Pipe
     pipe_list = move_pipe(pipe_list)
     draw_pipe(pipe_list)
-    # Floor giảm vị trí ban đầu 1 đơn vị
+    # Floor
     floor_x_position -=1
     double_floor()
-    # sàn thứ 2 chạy xong thì sàn thứ 1 thay lên vtri sàn thứ2 và lùi ngược lại
+    
     if floor_x_position <= -432:
          floor_x_position=0
-        # hiện lên màng hình
+       
     pygame.display.update()
     clock.tick(120)
