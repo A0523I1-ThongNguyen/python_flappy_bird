65285f4b92043f34af18bc6e701ee80af6dded06
Thong <you@example.com>
2024-07-28 20:05:57 +0700

    up2

--- game.py
+++ game.py
@@ -2,5 +2,12 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((432,768))
 
+# vòng lặp sự kiện, lấy all event trong game diễn ra
 while True:
-    
+    for event in pygame.event.get():
+        # tạo phím ấn thoát game
+        if event.type == pygame.QUIT:
+            # phím thoát
+            pygame.quit() 
+        # hiện lên màng hình
+    pygame.display.update()
